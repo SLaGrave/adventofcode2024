3bf8246cbf3428761fde7084e549531e5f9ff9ef
Sam LaGrave <srlagrave@gmail.com>
2024-11-30 23:13:46 -0600

    Day01 with comments.

--- day01/solution.py
+++ day01/solution.py
@@ -1,38 +1,45 @@
 def clean_data(data: list) -> list:
+    # Out is the two vertical lists
     out = [[], []]
     data = [line.strip() for line in data]
     for line in data:
         x = line.split("   ")
         out[0].append(int(x[0]))
         out[1].append(int(x[1]))
-
     return out
 
 
 def part1(data: list):
+    # S is the running total
     s = 0
+    # X is the left list, Y is the right list - both are sorted
     x = sorted(data[0])
     y = sorted(data[1])
+    # For each index, add the distance to the running total
     for i in range(len(x)):
         s += abs(x[i] - y[i])
-    
     return s
 
 
 def part2(data: list):
+    # S is the running total
     s = 0
+    # X is the left list, Y is the right list - both are sorted
     x = sorted(data[0])
     y = sorted(data[1])
+    # Scores is a dictionary of how many times each number (up to the highest value in the left list)
+    # appears in the right list.
+    # This is done naively
     scores = {}
-    print(max(x))
     for i in range(max(x) + 1):
         scores[i] = 0
         for item in y:
             if item == i:
                 scores[i] += 1
+    # For each number in the left list, add to the running
+    # total that value * it's "score"
     for item in x:
         s += item * scores[item]
-    
     return s
 
 
