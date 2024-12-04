22cab581a34a5bcf11655b6d4e443372fbc5ee68
Sam LaGrave <srlagrave@gmail.com>
2024-12-03 19:24:06 -0600

    Rust day 3 works.

--- day03/rust-day03/src/aoc_parser.rs
+++ day03/rust-day03/src/aoc_parser.rs
@@ -3,28 +3,70 @@ use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "aoc.pest"]
-pub struct InnerAOCParser;
+pub struct PestParser;
 
-#[derive(Debug)]
 pub struct AOCParser {
     do_dont: bool,
+    enabled: bool,
+    total: i32,
 }
 
 impl AOCParser {
     pub fn new() -> Self {
         Self {
             do_dont: true,
+            enabled: true,
+            total: 0,
         }
     }
 
-    pub fn new_part1() -> Self {
+    pub fn new_day3_part1() -> Self {
         Self {
             do_dont: false,
+            enabled: true,
+            total: 0,
         }
     }
 
-    pub fn parse(&self, code: &str) {
-        let successful_parse = InnerAOCParser::parse(Rule::aoc_program, code);
-        println!("{:?}", successful_parse);
+    pub fn get_total(&self) -> i32 {
+        self.total
+    }
+
+    pub fn parse(mut self, code: &str) -> i32 {
+        let program = PestParser::parse(Rule::aoc_program, code)
+            .expect("Unsuccessful parse")
+            .next()
+            .unwrap();
+        for item in program.into_inner() {
+            match item.as_rule() {
+                Rule::aoc_mul => {
+                    if self.enabled {
+                        // Parse lhs & rhs
+                        // We're happen to trust that there can ONLY be
+                        // EXACTLY two ints here, if Pest did it's job.
+                        let mut values: Vec<i32> = vec![];
+                        for value in item.into_inner() {
+                            let v = str::parse::<i32>(value.as_span().as_str()).unwrap();
+                            values.push(v);
+                        }
+                        // Multiply them together
+                        // and add to total
+                        self.total += values.get(0).unwrap() * values.get(1).unwrap()
+                    }
+                }
+                Rule::aoc_do => {
+                    if self.do_dont {
+                        self.enabled = true;
+                    }
+                }
+                Rule::aoc_dont => {
+                    if self.do_dont {
+                        self.enabled = false;
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+        self.get_total()
     }
 }
--- day03/rust-day03/src/main.rs
+++ day03/rust-day03/src/main.rs
@@ -1,9 +1,13 @@
 mod aoc_parser;
 
 fn main() {
-    let part1 = aoc_parser::AOCParser::new_part1();
-    part1.parse("mul(12,165)asdad");
+    let input = include_str!("../../input.txt");
+
+    let part1 = aoc_parser::AOCParser::new_day3_part1();
+    let part1_answer = part1.parse(input);
+    println!("Part 1:\n{}", part1_answer);
 
     let part2 = aoc_parser::AOCParser::new();
-    part2.parse("mul(12,165)asdad");
+    let part2_answer = part2.parse(input);
+    println!("Part 2:\n{}", part2_answer);
 }
