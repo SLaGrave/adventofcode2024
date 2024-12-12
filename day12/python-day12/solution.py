b445642bd4f2ffc08ab15ee178e4f669cf22905c
Sam LaGrave <srlagrave@gmail.com>
2024-12-12 09:04:35 -0600

    Day 12 part 1

---
+++ day12/python-day12/solution.py
@@ -0,0 +1,101 @@
+def clean_data(data: list) -> list:
+    data = [line.strip() for line in data]
+    data = [[[k, None, None] for k in q] for q in data]
+    return data
+
+
+DIRS = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+]
+
+def get(x, y, data):
+    if x < 0 or x >= len(data[0]) or y < 0 or y >= len(data):
+        return [None, None, None]
+    return data[y][x]
+
+
+def part1(data: list):
+    s = 0
+    idx = 0
+    sames = list()
+    for y in range(len(data)):
+        for x in range(len(data[y])):
+            item = data[y][x]
+            empty_edges = 0
+            possible_group = list()
+            for dir in DIRS:
+                neighbor = get(x+dir[0], y+dir[1], data)
+                if item[0] == neighbor[0]:
+                    if neighbor[2] is not None:
+                        possible_group.append(neighbor[2])
+                else:
+                    empty_edges += 1
+            if len(set(possible_group)) > 1:
+                found = False
+                tmp = set()
+                for i in range(len(sames)):
+                    for g in possible_group:
+                        if g in sames[i]:
+                            found=True
+                            tmp.add(i)
+                if not found:
+                    sames.append(possible_group)
+                else:
+                    tmp = sorted(list(tmp))
+                    for i in range(len(tmp)):
+                        if i == 0: continue
+                        sames[tmp[0]].extend(sames[tmp[i]])
+                        sames.pop(tmp[i])
+                    sames[tmp[0]].extend(possible_group)
+                    sames[tmp[0]] = list(set(sames[tmp[0]]))
+            if len(possible_group) == 0:
+                item[2] = idx
+                idx += 1
+            else:
+                item[2] = possible_group[0]
+            item[1] = empty_edges
+    costs = list()
+    sames = [sorted(q) for q in sames]
+    for same in sames:
+        for i in same:
+            for s2 in sames:
+                if s2 == same:
+                    continue
+                if i in s2:
+                    raise Exception("OH")
+    for _ in range(idx):
+        costs.append([0, 0])
+    for y in range(len(data)):
+        for x in range(len(data[y])):
+            item = data[y][x]
+            for same in sames:
+                if item[2] in same:
+                    item[2] = same[0]
+            costs[item[2]][0] += 1
+            costs[item[2]][1] += item[1]
+    for item in costs:
+        s += item[0] * item[1]
+    return s
+
+
+def part2(data: list):
+    s = 0
+    for line in data:
+        # do something
+        ...
+    
+    return s
+
+
+if __name__ == "__main__":
+    with open("../input.txt", "r") as f:
+        data = f.readlines()
+    data = clean_data(data)
+    p1 = part1(data.copy())
+    print(f"Part 1 solution:\n{p1}")
+    print("====================================")
+    p2 = part2(data.copy())
+    print(f"Part 2 solution:\n{p2}")
