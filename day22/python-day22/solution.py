9c0aa4d910439fe4634a86da1de3b39dda2c937e
Sam LaGrave <srlagrave@gmail.com>
2024-12-22 10:39:41 -0600

    Day 22 part 1 done. Part 2 looks hard.

--- day22/python-day22/solution.py
+++ day22/python-day22/solution.py
@@ -1,16 +1,42 @@
-def clean_data(data: list) -> list:
-    data = [line.strip() for line in data]
+from functools import cache
 
+
+def clean_data(data: list) -> list:
+    data = [int(line.strip()) for line in data]
     return data
 
 
+@cache
+def mix(secret, result):
+    return secret ^ result
+
+
+@cache
+def prune(secret):
+    return secret % 16777216
+
+
+@cache
+def evolve(secret):
+    # Step 1
+    result = secret * 64
+    secret = mix(secret, result)
+    secret = prune(secret)
+    # Step 2
+    result = int(secret / 32)
+    secret = prune(mix(secret, result))
+    # Step 3
+    result = secret * 2048
+    secret = prune(mix(secret, result))
+    return secret
+
+
 def part1(data: list):
-    s = 0
-    for line in data:
-        # do something
-        ...
-    
-    return s
+    secrets = data.copy()
+    for idx in range(len(data)):
+        for _ in range(2000):
+            secrets[idx] = evolve(secrets[idx])
+    return sum(secrets)
 
 
 def part2(data: list):
@@ -23,6 +49,13 @@ def part2(data: list):
 
 
 if __name__ == "__main__":
+    # Tests
+    assert mix(42, 15) == 37
+    assert prune(100000000) == 16113920
+    assert evolve(123) == 15887950
+    assert evolve(evolve(123)) == 16495136
+
+    # Logic
     with open("../input.txt", "r") as f:
         data = f.readlines()
     data = clean_data(data)
